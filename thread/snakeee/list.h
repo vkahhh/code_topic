15e1dcb41da24b90acf5db13a1fb281ca2f5c09e
vkahhh <7.luhan.m.luexolu@gmail.com>
2022-05-17 21:35:39 -0700

    them con ran

---
+++ thread/snakeee/list.h
@@ -0,0 +1,39 @@
+
+ * @file list.h
+ * Header for list implementation
+ */
+
+
+
+#ifndef __LIST_H__
+#define __LIST_H__
+/*Double linked-list*/
+
+/*Create a node*/
+struct list_node {
+	void *data;
+	struct list_node *next;
+	struct list_node *prev;
+};
+typedef struct list_node NODE;
+
+/*create a list*/
+typedef struct {
+	NODE *firstNode;
+	NODE *lastNode;
+} LIST;
+
+void list_init(LIST *list);
+void list_insert_after(LIST *list, NODE *node, void *datum);
+void list_insert_before(LIST *list, NODE *node, void *datum);
+void list_prepend(LIST *list, void *datum);
+void list_append( LIST *list, void *datum );
+void list_map( LIST *list, void (*f)(void *) );
+void *list_front( LIST *list );
+void *list_remove(LIST *list, NODE *node);
+void list_delete( LIST *list );
+int list_empty(LIST *list);
+
+
+#endif
+
